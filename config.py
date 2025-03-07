b0f137e91e9e426fcc169cb994119c00d252b4c0
tgseries2 <tgseries.2@gmail.com>
2025-03-07 06:09:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+CONFIG = {
+    "RPC_ENDPOINT": "https://eth-mainnet.alchemyapi.io/v2/-OhPvvb9D6wDOYAyOgyD60oHMKteW6nM",
+    "PRIVATE_KEY": "e68e86152eba78813001a01e2d09f6ebd6d91f5be86b98f7eec4be40f62274d1",
+    "WALLET_ADDRESS": "0x8A7c84C0291d271f22ab72aDeA27417670F85FA1",
+    "FLASHBOTS_RELAY": "https://relay.flashbots.net",
+    "CONTRACT_ADDRESS": "0xYourDeployedContractAddress"  # Replace after deploying
+}
